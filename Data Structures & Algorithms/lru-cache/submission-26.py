7fa62dd9a42597c3dfb45c21bdea8c7cb6378d44
DanielRomanini <105990749+DanielRomanini@users.noreply.github.com>
2026-09-02 17:52:59 -0400

    Add: lru-cache - submission-26

---
+++ Data Structures & Algorithms/lru-cache/submission-26.py
@@ -0,0 +1,25 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.hashmap = {}
+        self.limit = capacity
+        self.queue = deque()
+
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.hashmap[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+
+        if(key in self.queue):
+            self.queue.remove(key)
+        if (len(self.queue)>=self.limit):
+            del self.hashmap[self.queue.popleft()]
+
+        self.queue.append(key)
+        self.hashmap[key] = value
+        
