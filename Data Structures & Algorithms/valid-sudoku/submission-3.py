e46af579fc762b6c5c142655a61f902a2813416f
DanielRomanini <105990749+DanielRomanini@users.noreply.github.com>
2026-09-07 18:11:40 -0400

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        def isValidStraight(line):
+            hashtable = {}
+            for num in line:
+                if num not in hashtable:
+                    hashtable[num] = 1
+                else:
+                    hashtable[num] += 1
+            if '.' in hashtable:
+                return(len(line) == (len(hashtable)+hashtable["."]-1))
+            return(len(line) == len(hashtable))
+
+        for row in board:
+            if not isValidStraight(row):
+                return False
+        
+        for col in zip(*board):
+            if not isValidStraight(col):
+                return False
+
+        r=3
+        c=3
+        while(True):
+
+            temp = []
+            for i in range(r-3, r):
+                for j in range(c-3,c):
+                    temp.append(board[i][j])
+
+            if not isValidStraight(temp):
+                return False
+                
+            if c>=9 and r>=9:
+                break
+            
+            if(r<9):
+                r+=3
+            else:
+                r = 3
+                c+=3
+
+
+
+
+        return True
