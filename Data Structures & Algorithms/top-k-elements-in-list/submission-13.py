3ca791a032a0a726984703c3cf9f2fb7ffb1668e
DanielRomanini <105990749+DanielRomanini@users.noreply.github.com>
2026-09-09 20:21:56 -0400

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashtable = {}
+        for num in nums:
+            if num not in hashtable:
+                hashtable[num] = 1
+            else:
+                hashtable[num] += 1
+        
+        temp = []
+
+        while(k>0):
+            greatest = -1
+            greatestIndex = -1
+            for num in hashtable:
+                if hashtable[num] > greatest:
+                    greatest = hashtable[num]
+                    greatestIndex = num
+            temp.append(greatestIndex)
+            hashtable[greatestIndex] = -2
+            greatest = -1
+            k-=1
+        return temp
