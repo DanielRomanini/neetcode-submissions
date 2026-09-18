0b1e298dc53f168a44a79d0df0900904862b4622
DanielRomanini <105990749+DanielRomanini@users.noreply.github.com>
2026-09-18 11:17:32 -0400

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = {}
+        for i in range(len(nums)):
+            if nums[i] not in hashtable:
+                hashtable[nums[i]] = i
+            
+        for i in range(len(nums)):
+            needed = target - nums[i]
+            if(needed in hashtable and i!=hashtable[needed]):
+                arr = [i,hashtable[needed]]     
+                arr.sort()
+                return arr
